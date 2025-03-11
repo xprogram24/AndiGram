7552740347d9aa285697d9763d83f7957a04c18f
xprogram24 <cexcel58@gmail.com>
2025-03-11 14:00:00 +0100

    added an on click for the responsive sidbar

---
+++ maindashboard/dashboard.js
@@ -0,0 +1,25 @@
+let sidebarbutton = document.getElementById('logo');
+let sidebar = document.getElementById('sidebar');
+let search = document.createElement('div');
+search.classList.add('search')
+let searchbox = document.createElement('input');
+
+function createSearch() {
+    searchbox.type = 'search';
+    searchbox.name = 'search';
+    searchbox.id = 'search-box';
+    searchbox.placeholder = 'search';
+    searchbox.classList.add('search')
+    search.appendChild(searchbox);
+}
+
+sidebarbutton.addEventListener('click', function() {
+    if (sidebar.style.width === '300px') {
+        sidebar.style.width = '120px';
+        search.remove();
+    } else {
+        sidebar.style.width = '300px';
+        createSearch();
+        sidebarbutton.insertAdjacentElement("afterend", search);
+    }
+});
